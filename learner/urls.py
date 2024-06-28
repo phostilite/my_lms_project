e54fb498acd2d3a72a1109cdf9fe8486d3fd27e0
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-06-28 10:58:43 +0530

    add leaderboard

--- learner/urls.py
+++ learner/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name='learner_dashboard'),
     path('calendar/', views.calendar, name='learner_calendar'),
     path('my_courses/', views.my_courses, name='learner_my_courses'),
-     path('my_certificates/', views.my_certificates, name='learner_my_certificates'),
-     path('my_badge/', views.my_badge, name='learner_my_badge')
+    path('my_certificates/', views.my_certificates, name='learner_my_certificates'),
+    path('my_badge/', views.my_badge, name='learner_my_badge'),
+    path('leaderboard/', views.leaderboard, name='learner_leaderboard')
 ]
