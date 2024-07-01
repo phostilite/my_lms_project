bcf56e576dda58b17de47e58f7f8cceb8cd3fffc
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-07-01 12:49:31 +0530

    update: requirements

--- administrator/urls.py
+++ administrator/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('calendar/', views.calendar, name='administrator_calendar'),
     path('course_list/', views.course_list, name='course_list'),
     path('upload_course/', views.upload_course, name='upload_course'),
+    path('leaderboard/', views.leaderboard, name='administrator_leaderboard'),
 ]
