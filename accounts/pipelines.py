795314bca5cf896bf52afed6673457029a81aa55
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-07-05 12:44:43 +0530

    updated: redirect url

--- accounts/pipelines.py
+++ accounts/pipelines.py
@@ -5,6 +5,8 @@ from social_core.pipeline.social_auth import social_details, associate_by_email
 from social_django.utils import load_strategy
 import logging
 
+from .utils import redirect_based_on_group
+
 logger = logging.getLogger(__name__)
 
 User = get_user_model()
@@ -16,23 +18,11 @@ def social_auth_user(backend, details, response, *args, **kwargs):
     if not email:
         messages.error(request, "Email not found in your Google account.")
         return redirect('login')
-    
     try:
         user = User.objects.get(email=email)
-        # This is where you authenticate the user
-        # Make sure the user is active or any other checks you want to perform
         if user.is_active:
             login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            
-            if user.groups.filter(name='learner').exists():
-                return redirect('learner_dashboard')
-            elif user.groups.filter(name='instructor').exists():
-                return redirect('instructor_dashboard')
-            elif user.groups.filter(name='administrator').exists():
-                return redirect('administrator_dashboard')
-            else:
-                messages.info(request, 'Welcome! Please contact an administrator to assign you a role.')
-                return redirect('landing_page')
+            return redirect_based_on_group(request, user)
         else:
             messages.error(request, "Your account is not active. Please contact support.")
             return redirect('login')
