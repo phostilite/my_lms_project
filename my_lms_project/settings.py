4e169acdebd94d3c6ed03eaf349eab32e90b07c6
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-07-07 17:03:41 +0530

    update: hide django secret key

--- my_lms_project/settings.py
+++ my_lms_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ex+p6(xq8#!*-ei^!@mln^8o9*b#yiy40#4g2kp*vcx=lnpe^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
